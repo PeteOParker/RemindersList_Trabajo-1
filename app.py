a2aca3523713dc23aaf80601338bc7ca33fd927b
Luis Alejandro Salcedo <luis.salcedo@virginiogomez.cl>
2025-04-11 22:23:08 -0400

    feat: caracteristica de editar Reminders agregada. Interfaz de usuario y API completadas.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Trabajo #1 de Programación Orientado a Objetos.
-# Luis Alejandro Salcedo.
 from flask import Flask, request, render_template, redirect, flash
 from datetime import datetime
 
@@ -8,13 +6,6 @@ app.secret_key = 'd94231c751543589c4619656fb1781f28bc9452632e5977ef76660c352bc5d
 
 reminders = []
 idActual = 1
-
-@app.before_request
-def method_override():
-    if request.method == "POST" and "_method" in request.form:
-        method = request.form["_method"].upper()
-        if method in ["PUT", "PATCH", "DELETE"]:
-            request.environ["REQUEST_METHOD"] = method
 
 @app.route("/")
 def index():
@@ -27,11 +18,9 @@ def api_reminders():
 # END POINT: Agregar Recordatorio
 @app.route("/api/reminders", methods=["POST"])
 def api_add_reminder():
-    # Variables
     global idActual
     createdAt = datetime.now().timestamp()
 
-    # Validaciones de campos vacios
     try:
         texto = request.form["content"]
     except ValueError:
@@ -45,7 +34,6 @@ def api_add_reminder():
     if texto.strip() == "":
         return "El content no puede estar vacío", 400
 
-    # Validacion de Important
     if importante is None:
         importante = False
     elif importante == "true" or importante == "True":
@@ -55,15 +43,53 @@ def api_add_reminder():
     else:
         return "El valor de important debe ser True o False", 400
 
-    # Agregar recordatorio
     new_reminder = { "id": idActual, "content": texto, "important": importante, "createdAt": createdAt }
     reminders.append(new_reminder)
     idActual += 1
-
+    reminders.sort(key=lambda x: not x["important"])
     return redirect("/")
 
-# END POINT: Eliminar Recordatorio
-@app.route("/api/reminders/<int:id>", methods=["POST", "DELETE"])
+# END POINT: Actualizar Recordatorio
+@app.route("/api/reminders/<int:id>", methods=["POST", "PATCH"])
+def actualizar_reminder(id):
+    content = request.form.get("content", "").strip()
+    important = request.form.get("important", "false").lower()
+
+    for reminder in reminders:
+        if reminder["id"] == id:
+            if content:
+                if len(content) > 120:
+                    flash("El contenido no puede superar los 120 caracteres", "danger")
+                    return redirect("/")
+                reminder["content"] = content
+
+            if important == "true":
+                reminder["important"] = True
+            elif important == "false":
+                reminder["important"] = False
+            else:
+                flash("El valor de 'importante' debe ser True o False", "danger")
+                return redirect("/")
+            reminders.sort(key=lambda x: not x["important"])
+            flash("Recordatorio actualizado exitosamente", "success")
+            return redirect("/")
+
+    flash("Recordatorio no encontrado", "danger")
+    return redirect("/")
+
+# END POINT: Borrar Recordatorio
+@app.route("/api/reminder/<int:id>", methods=["POST", "DELETE"])
+def borrar_reminder(id):
+    for index, reminder in enumerate(reminders):
+        if reminder["id"] == id:
+            del reminders[index]
+            flash("Recordatorio eliminado exitosamente.", "success")
+            return redirect("/")
+    flash("Recordatorio no encontrado.", "danger")
+    return redirect("/")
+
+# END POINT: Borrar Recordatorio (API)
+@app.route("/api/reminders/<int:id>", methods=["DELETE"])
 def api_borrar_reminder(id):
     for index, reminder in enumerate(reminders):
         if reminder["id"] == id:
